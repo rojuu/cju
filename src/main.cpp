e9b5545e8615c1caeddb0ec10e01581efb080e88
Roni Juppi <roju269@gmail.com>
2019-09-11 21:04:37 +0300

    Reading a file

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
-int main(int, char**)
+#include <cassert>
+
+struct Token
 {
-    std::cout << "Hello world\n";
-    return 0;
+};
+
+std::vector<Token> tokenizeFile(const std::string &fileContents)
+{
+    (void)fileContents;
+    // Tokenize the file here...
+    return {};
+}
+
+bool readEntireFile(const std::string &filename, std::string &fileContentsOut)
+{
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        fileContentsOut = "";
+        return false;
+    }
+
+    std::string fileContents((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    file.close();
+
+    fileContentsOut = fileContents;
+    return true;
+}
+
+int main(int argc, char **argv)
+{
+    assert(argc == 2);
+
+    std::string fileContents;
+    if (!readEntireFile(argv[1], fileContents)) {
+        std::cerr << "Failed to read file" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    tokenizeFile(fileContents);
+    std::cout << fileContents << std::endl;
+
+    return EXIT_SUCCESS;
 }
