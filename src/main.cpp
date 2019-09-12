f1ee4323cdb48e32102e781dfcbd9d3be290f77d
Roni Juppi <roju269@gmail.com>
2019-09-12 19:13:58 +0300

    Fix line ending problem, and probably save some memory in the progress

--- src/main.cpp
+++ src/main.cpp
@@ -8,63 +8,28 @@ struct Token
 {
 };
 
-std::vector<std::string> splitIntoLines(const std::string &fileContents)
+std::vector<Token> tokenizeFile(std::ifstream &file)
 {
-    std::vector<std::string> result;
+    file.seekg(0);
 
-    size_t lineStartIndex = 0;
-    size_t lineEndIndex = 0;
-    while (lineEndIndex != fileContents.npos) {
-        lineEndIndex = fileContents.find("\n", lineStartIndex); // TODO: Make work with CRLF as well
-        std::string line(fileContents, lineStartIndex, lineEndIndex - lineStartIndex);
-        if (line.size() > 0) {
-            result.push_back(line);
-        }
-        lineStartIndex = lineEndIndex + 1;
+    std::string line;
+    while (std::getline(file, line)) {
+        std::cout << line.size() << " " << line << std::endl;
     }
-
-    return result;
-}
-
-std::vector<Token> tokenizeFile(const std::string &fileContents)
-{
-    auto lines = splitIntoLines(fileContents);
-
-    std::cout << "line count " << lines.size() << std::endl;
-    for (auto &line : lines) {
-        std::cout << line << std::endl;
-    }
-    // Tokenize the file here...
 
     return {};
-}
-
-bool readEntireFile(const std::string &filename, std::string &fileContentsOut)
-{
-    std::ifstream file(filename);
-    if (!file.is_open()) {
-        fileContentsOut = "";
-        return false;
-    }
-
-    std::string fileContents((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-    file.close();
-
-    fileContentsOut = fileContents;
-    return true;
 }
 
 int main(int argc, char **argv)
 {
     assert(argc == 2);
 
-    std::string fileContents;
-    if (!readEntireFile(argv[1], fileContents)) {
-        std::cerr << "Failed to read file" << std::endl;
-        return EXIT_FAILURE;
+    std::ifstream file(argv[1]);
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file " << argv[1];
     }
 
-    tokenizeFile(fileContents);
+    tokenizeFile(file);
 
     std::cout << "Done" << std::endl;
 
