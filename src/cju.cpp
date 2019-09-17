ee5650401a2edb042e1faec28ba253c9fdc95d30
Roni Juppi <roju269@gmail.com>
2019-09-17 15:23:12 +0300

    Lexer going along nicelyStill need number literals, stringliterals, comment parsing, and# define etc parsing

--- src/cju.cpp
+++ src/cju.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <vector>
+#include <regex>
 
 #include <cassert>
 
@@ -19,13 +21,39 @@ namespace cju
  */
 enum class TokenType
 {
+    NONE,
     IDENTIFIER,
     FRACTION,
     NUMBER,
+    STRING_LITERAL,
     OPERATOR,
     PAREN,
+    SEPARATOR,
     SCOPE,
 };
+
+std::string tokenTypeToString(TokenType type) {
+    switch(type) {
+    case TokenType::NONE:
+        return "NONE";
+    case TokenType::IDENTIFIER:
+        return "IDENTIFIER";
+    case TokenType::FRACTION:
+        return "FRACTION";
+    case TokenType::NUMBER:
+        return "NUMBER";
+    case TokenType::STRING_LITERAL:
+        return "STRING_LITERAL";
+    case TokenType::OPERATOR:
+        return "OPERATOR";
+    case TokenType::PAREN:
+        return "PAREN";
+    case TokenType::SEPARATOR:
+        return "SEPARATOR";
+    case TokenType::SCOPE:
+        return "SCOPE";
+    }
+}
 
 struct Token
 {
@@ -33,10 +61,14 @@ struct Token
     std::string value;
 };
 
-void eatWhiteSpace(char *&ptr)
+void findNextWord(char *&beginPtr, char *&endPtr)
 {
-    while (*ptr == ' ') {
-        ptr++;
+    while (*beginPtr == ' ') {
+        ++beginPtr;
+    }
+    endPtr = beginPtr;
+    while (*endPtr != ' ' && *endPtr != 0) {
+        ++endPtr;
     }
 }
 
@@ -48,8 +80,83 @@ std::vector<Token> tokenizeFile(std::ifstream &file)
 
     std::string line;
     while (std::getline(file, line)) {
-        char* ptr = &line[0];
-        eatWhiteSpace(ptr);
+        // First split line by whitespace
+        std::istringstream iss(line);
+        std::vector<std::string> tokenStrings(
+            (std::istream_iterator<std::string>(iss)),
+            (std::istream_iterator<std::string>()));
+        for (auto tokenStr : tokenStrings) {
+            auto begin = tokenStr.begin();
+            auto end = tokenStr.end();
+            // Tokenize each part accordingly
+            while (begin != end) {
+        /*
+        * number = (digits)+ | ((digits)+\.(digits)*)
+        */
+        /*
+            FRACTION,
+            NUMBER,
+            STRING_LITERAL,
+            */
+           /*
+            This stuff would sound like a precompiler sort of thing:
+            comment blocks,
+            line comments
+            # defines /etc.
+           */
+
+                std::string curStr(begin, end);
+                Token token{};
+                std::smatch baseMatch;
+                if (std::regex_match(curStr, baseMatch, std::regex("([a-zA-Z]+[a-zA-Z0-9_]*)(.*)"))) {
+                    auto matchLenght = baseMatch[1].length();
+                    token.type = TokenType::IDENTIFIER;
+                    token.value = std::string(begin, begin + matchLenght);
+                    begin += matchLenght;
+                } else if (*begin == '(' || *begin == ')') {
+                    token.type = TokenType::PAREN;
+                    token.value = std::string(begin, begin + 1);
+                    begin += 1;
+                } else if (*begin == '{' || *begin == '}') {
+                    token.type = TokenType::SCOPE;
+                    token.value = std::string(begin, begin + 1);
+                    begin += 1;
+                } else if(*begin == ',' || *begin == ';') {
+                    token.type = TokenType::SEPARATOR;
+                    token.value = std::string(begin, begin + 1);
+                    begin += 1;
+                } else if(*begin == '+' ||
+                          *begin == '-' ||
+                          *begin == '*' ||
+                          *begin == '/' ||
+                          *begin == '>' ||
+                          *begin == '<' ||
+                          *begin == '=' ||
+                          *begin == '|' ||
+                          *begin == '&') {
+                    token.type = TokenType::OPERATOR;
+                    token.value = std::string(begin, begin +1);
+                    begin += 1;
+                } else if((begin[0] == '>' && begin[1] == '=') ||
+                          (begin[0] == '<' && begin[1] == '=') ||
+                          (begin[0] == '=' && begin[1] == '=') ||
+                          (begin[0] == '|' && begin[1] == '|') ||
+                          (begin[0] == '&' && begin[1] == '&') ||
+                          (begin[0] == '<' && begin[1] == '<') ||
+                          (begin[0] == '>' && begin[1] == '>')) {
+                    token.type = TokenType::OPERATOR;
+                    token.value = std::string(begin, begin + 2);
+                    begin += 2;
+                } else {
+                    token.type = TokenType::NONE;
+                    ++begin;
+                }
+
+                if (token.type != TokenType::NONE) {
+                    tokens.push_back(token);
+                }
+            }
+        }
     }
 
     return tokens;
@@ -65,7 +172,11 @@ int run(int argc, char **argv)
         return 1;
     }
 
-    tokenizeFile(file);
+    auto tokens = tokenizeFile(file);
+
+    for (auto token : tokens) {
+        std::cout << tokenTypeToString(token.type) << " " << token.value << std::endl;
+    }
 
     std::cout << "Done" << std::endl;
 
--- test.c
+++ test.c
@@ -3,7 +3,12 @@ int add(int a, int b)
     return a + b;
 }
 
+int sub_numbers(int a, int b)
+{
+    return a - b;
+}
+
 int main(void)
 {
-    return add(1, 2);
+    return add(1, sub_numbers(3 , 4));
 }
