b885290601c989adbab9e4f06b03d19a8756d323
Roni Juppi <roju269@gmail.com>
2019-09-26 19:05:26 +0300

    Using a lexing library instead
    Turns out lexing is very tedious, but not that interesting

--- src/cju.cpp
+++ src/cju.cpp
@@ -7,201 +7,57 @@
 
 #include <cassert>
 
+#define LEXER_IMPLEMENTATION
+#define LEXER_USE_ASSERT
+#define LEXER_SIZE_TYPE size_t
+#include "lexer.h"
+
 namespace cju
 {
 
-/**
- * letter = [a-zA-Z]
- * digit = [0-9]
- * digits = digit digit*
- * identifier = [_ | letter](letter | digit | _).*
- * number = (digits)+ | ((digits)+\.(digits)*)
- * operator = + | - | * | / | > | >= | < | <= | = | == | | | || | & | && | << | >> | %
- * parenthesis = ( | )
- * scope = { | }
- */
-enum class TokenType
-{
-    NONE,
-    IDENTIFIER,
-    FRACTION,
-    INTEGER,
-    STRING_LITERAL,
-    OPERATOR,
-    PAREN,
-    SEPARATOR,
-    SEMICOLON,
-    SCOPE,
-};
-
-std::string tokenTypeToString(TokenType type) {
+std::string tokenTypeToString(lexer_token_type type) {
     switch(type) {
-    case TokenType::NONE:
-        return "NONE";
-    case TokenType::IDENTIFIER:
-        return "IDENTIFIER";
-    case TokenType::FRACTION:
-        return "FRACTION";
-    case TokenType::INTEGER:
-        return "INTEGER";
-    case TokenType::STRING_LITERAL:
-        return "STRING_LITERAL";
-    case TokenType::OPERATOR:
-        return "OPERATOR";
-    case TokenType::PAREN:
-        return "PAREN";
-    case TokenType::SEPARATOR:
-        return "SEPARATOR";
-    case TokenType::SEMICOLON:
-        return "SEMICOLON";
-    case TokenType::SCOPE:
-        return "SCOPE";
+    case LEXER_TOKEN_STRING:
+        return "LEXER_TOKEN_STRING";
+    case LEXER_TOKEN_LITERAL:
+        return "LEXER_TOKEN_LITERAL";
+    case LEXER_TOKEN_NUMBER:
+        return "LEXER_TOKEN_NUMBER";
+    case LEXER_TOKEN_NAME:
+        return "LEXER_TOKEN_NAME";
+    case LEXER_TOKEN_PUNCTUATION:
+        return "LEXER_TOKEN_PUNCTUATION";
     }
 }
 
-struct Token
+void lexerLogCallback(void*, enum lexer_log_level logLevel, lexer_size line, const char *msg)
 {
-    TokenType type;
-    std::string value;
-};
-
-void findNextWord(char *&beginPtr, char *&endPtr)
-{
-    while (*beginPtr == ' ') {
-        ++beginPtr;
+    std::string errorOutput;
+    switch(logLevel) {
+    case LEXER_WARNING:
+        errorOutput+="LEXER WARNING ";
+    break;
+    case LEXER_ERROR:
+        errorOutput+="LEXER ERROR ";
+    break;
     }
-    endPtr = beginPtr;
-    while (*endPtr != ' ' && *endPtr != 0) {
-        ++endPtr;
-    }
+    errorOutput+="on line ";
+    errorOutput+=line;
+    errorOutput+=msg;
+    std::cout << errorOutput << std::endl;
 }
 
-bool tokenizeFile(std::ifstream &file, std::vector<Token> &tokens)
+bool tokenizeFile(const std::string &fileContents, std::vector<lexer_token> &tokens)
 {
-    file.seekg(0);
+    lexer lexer;
+    lexer_init(&lexer, fileContents.c_str(), fileContents.size(), nullptr, nullptr, nullptr);
 
-    bool inCommentBlock = false;
-
-    std::string line;
-    while (std::getline(file, line)) {
-        // First split line by whitespace
-        std::istringstream iss(line);
-        std::vector<std::string> tokenStrings(
-            (std::istream_iterator<std::string>(iss)),
-            (std::istream_iterator<std::string>()));
-        for (auto tokenStr : tokenStrings) {
-            auto begin = tokenStr.begin();
-            auto end = tokenStr.end();
-            // Tokenize each part accordingly
-            while (begin != end) {
-                /*
-                    number = (digits)+ | ((digits)+\.(digits)*)
-                */
-                /*
-                    FRACTION,
-                    NUMBER,
-                    STRING_LITERAL,
-                */
-                /*
-                    This stuff would sound like a precompiler sort of thing:
-                    comment blocks,
-                    line comments
-                    # defines /etc.
-                */
-
-                // Check for comments
-                if (!inCommentBlock) {
-                    if (((end-begin) >= 2) && (
-                        (begin[0] == '/' && begin[1] == '*')
-                    )) {
-                        inCommentBlock = true;
-                        begin+=2;
-                        continue;
-                    } else if (
-                        ((end-begin) >= 2) && (
-                            (begin[0] == '/' && begin[1] == '/')
-                    )) {
-                        // Comment, so skip the rest of the line
-                        goto next_line;
-                    }
-                } else {
-                    if (((end-begin) >= 2) && (
-                        (begin[0] == '*' && begin[1] == '/')
-                    )) {
-                        inCommentBlock = false;
-                        begin+=2;
-                        continue;
-                    } else {
-                        goto next_line;
-                    }
-                }
-
-                std::string curStr(begin, end);
-                Token token{};
-                std::smatch baseMatch;
-                if (std::regex_match(curStr, baseMatch, std::regex("([a-zA-Z]+[a-zA-Z0-9_]*)(.*)"))) {
-                    auto matchLenght = baseMatch[1].length();
-                    token.type = TokenType::IDENTIFIER;
-                    token.value = std::string(begin, begin + matchLenght);
-                    begin += matchLenght;
-                } else if (*begin == ';') {
-                    token.type = TokenType::SEMICOLON;
-                    token.value = std::string(begin, begin + 1);
-                    begin += 1;
-                } else if (*begin == '(' || *begin == ')') {
-                    token.type = TokenType::PAREN;
-                    token.value = std::string(begin, begin + 1);
-                    begin += 1;
-                } else if (*begin == '{' || *begin == '}') {
-                    token.type = TokenType::SCOPE;
-                    token.value = std::string(begin, begin + 1);
-                    begin += 1;
-                } else if(*begin == ',' || *begin == ';') {
-                    token.type = TokenType::SEPARATOR;
-                    token.value = std::string(begin, begin + 1);
-                    begin += 1;
-                } else if(
-                    ((end-begin) >= 2) && (
-                          (begin[0] == '>' && begin[1] == '=') ||
-                          (begin[0] == '<' && begin[1] == '=') ||
-                          (begin[0] == '=' && begin[1] == '=') ||
-                          (begin[0] == '|' && begin[1] == '|') ||
-                          (begin[0] == '&' && begin[1] == '&') ||
-                          (begin[0] == '<' && begin[1] == '<') ||
-                          (begin[0] == '>' && begin[1] == '>')
-                )) {
-                    token.type = TokenType::OPERATOR;
-                    token.value = std::string(begin, begin + 2);
-                    begin += 2;
-                } else if(*begin == '+' ||
-                          *begin == '-' ||
-                          *begin == '*' ||
-                          *begin == '/' ||
-                          *begin == '>' ||
-                          *begin == '<' ||
-                          *begin == '=' ||
-                          *begin == '|' ||
-                          *begin == '&') {
-                    token.type = TokenType::OPERATOR;
-                    token.value = std::string(begin, begin +1);
-                    begin += 1;
-                } else if (std::isalnum(*begin)) {
-                    token.type = TokenType::INTEGER;
-                    begin++;
-                } else {
-                    std::cout << "Unexpected token: " << *begin << std::endl;
-                    return false;
-                }
-
-                tokens.push_back(token);
-            }
-        }
-
-        next_line:;
+    lexer_token tok;
+    while(lexer_expect_any(&lexer, &tok)) {
+        tokens.push_back(tok);
     }
 
-    if (inCommentBlock) {
-        std::cout << "Expected '*/'" << std::endl;
+    if (lexer.error) {
         return false;
     }
 
@@ -218,13 +74,25 @@ int run(int argc, char **argv)
         return 1;
     }
 
-    std::vector<Token> tokens;
-    if (!tokenizeFile(file, tokens)) {
+    std::string fileContents;
+
+    file.seekg(0, std::ios::end);
+    fileContents.reserve(file.tellg());
+    file.seekg(0, std::ios::beg);
+
+    fileContents.assign((std::istreambuf_iterator<char>(file)),
+                       std::istreambuf_iterator<char>());
+
+    std::vector<lexer_token> tokens;
+    if (!tokenizeFile(fileContents, tokens)) {
+        std::cout << "Failed to tokenize file: " << argv[1] << std::endl;
         return 1;
     }
 
-    for (auto token : tokens) {
-        std::cout << tokenTypeToString(token.type) << " " << token.value << std::endl;
+    for (auto &token : tokens) {
+        std::cout << tokenTypeToString(token.type) << " "
+                  << std::string(token.str, token.str + token.len) << " "
+                  << std::endl;
     }
 
     std::cout << "Done" << std::endl;
