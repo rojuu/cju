dfe6841dd4ef5cf5c372b87b3f864944502fea2b
Roni Juppi <roju269@gmail.com>
2020-02-23 16:53:54 +0200

    Add some llvm headers and move them to common.h
    This is in preparation for outputting an .o file or something
    similar to that

--- src/ast.hpp
+++ src/ast.hpp
@@ -1,20 +1,5 @@
 #include "common.h"
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-parameter"
-#include <llvm/ADT/APFloat.h>
-#include <llvm/ADT/STLExtras.h>
-#include <llvm/IR/BasicBlock.h>
-#include <llvm/IR/Constants.h>
-#include <llvm/IR/DerivedTypes.h>
-#include <llvm/IR/Function.h>
-#include <llvm/IR/IRBuilder.h>
-#include <llvm/IR/LLVMContext.h>
-#include <llvm/IR/Module.h>
-#include <llvm/IR/Type.h>
-#include <llvm/IR/Verifier.h>
-#pragma GCC diagnostic pop
-
 namespace cju
 {
 
